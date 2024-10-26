a3a9518a701794cd3fe5f4c87f4b477174ddfe40
SupercraftD <dzhu700@gmail.com>
2024-10-26 14:18:10 -0400

    add firebase boilerplate to mainjs

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 import './style.css'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const firebaseConfig = {
+  apiKey: "AIzaSyCTdNmROBb4Qslb0W2vW_Pmzsss_xuztuo",
+  authDomain: "seestress-cc0de.firebaseapp.com",
+  projectId: "seestress-cc0de",
+  storageBucket: "seestress-cc0de.appspot.com",
+  messagingSenderId: "712991588919",
+  appId: "1:712991588919:web:0a0877f8ce2fa46a9435b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+onAuthStateChanged(auth, async(user)=>{
+  if (user){
+
+    //is signed in
+
+  }else{
+    
+    //is signed out
+
+  }
+})
